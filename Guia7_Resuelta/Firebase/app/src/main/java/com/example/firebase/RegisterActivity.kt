098be3afcf446dae86da7819cc5a2d98535ccb2d
Lucyvillalo <luciavillalobos2020@gmail.com>
2026-09-05 20:37:23 -0600

    guia7

---
+++ Guia7_Resuelta/Firebase/app/src/main/java/com/example/firebase/RegisterActivity.kt
@@ -0,0 +1,99 @@
+package com.example.firebase
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+
+    // Referencia del objeto FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
+    // Referencia a componentes del layout
+    private lateinit var buttonRegister: Button
+    private lateinit var textViewLogin: TextView
+
+    // Listener del estado de sesión (Parte 7)
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Inicializacion del objeto FirebaseAuth
+        auth = FirebaseAuth.getInstance()
+
+        buttonRegister = findViewById(R.id.btnRegister)
+        buttonRegister.setOnClickListener {
+            val email = findViewById<EditText>(R.id.txtEmail).text.toString()
+            val password = findViewById<EditText>(R.id.txtPassword).text.toString()
+            this.register(email, password)
+        }
+
+        textViewLogin = findViewById(R.id.textViewLogin)
+        textViewLogin.setOnClickListener {
+            this.goToLogin()
+        }
+
+        // Verificacion de sesión
+        this.checkUser()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        auth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        auth.removeAuthStateListener(authStateListener)
+    }
+
+    private fun checkUser() {
+        // Verificacion del ususario
+        authStateListener = FirebaseAuth.AuthStateListener { auth ->
+            if (auth.currentUser != null) {
+                // Cambiando la vista
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    private fun register(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(
+                    applicationContext,
+                    exception.localizedMessage,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+    }
+
+    private fun goToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ Guia7_Resuelta/FirebaseLogin/app/src/main/java/com/example/firebaselogin/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.firebaselogin
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_sign_out -> {
+                FirebaseAuth.getInstance()
+                    .signOut()
+                    .also {
+                        Toast.makeText(
+                            this,
+                            "Sesión cerrada",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
